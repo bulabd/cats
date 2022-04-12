551c9e8374f46479c305d1a12d32b557d7f1c7aa
bulabd <bulabd@outlook.com>
2022-04-12 18:46:18 +0000

    Performed Mocha and Chai tests

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -2,18 +2,21 @@
 const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, callback) {
-  console.log('breedDetailsFromFile: Calling readFile...');
+  // console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
+    // console.log("In readFile's Callback: it has the data.");
     if (!error) callback(data);
+    if (error) callback(undefined);
   });
 };
 
 const printOutCatBreed = breed => {
-  console.log('Return Value: ', breed);
+  // console.log('Return Value: ', breed);
 };
 
 
-breedDetailsFromFile('Bombay', printOutCatBreed);
+// breedDetailsFromFile('Bombay', printOutCatBreed);
 // console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+
+module.exports = breedDetailsFromFile;
 
